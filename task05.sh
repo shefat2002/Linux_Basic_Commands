7391e1dfa215dd2e50dcd4f628dbbd9f96bf66f9
Shefat <shefat2002@gmail.com>
2023-10-08 03:36:21 +0600

    Create sheel command of task 5.

---
+++ task05.sh
@@ -0,0 +1,4 @@
+sudo chmod 640 /home/shefat2002/os-concepts/*
+sudo chmod 640 /home/shefat2002/ubuntu-is-the-best/*
+sudo ls -l /home/shefat2002/os-concepts
+sudo ls -l /home/shefat2002/ubuntu-is-the-best
