65fc063c427adc678e90d3e90fae58306acd8c38
Shefat <shefat2002@gmail.com>
2023-10-08 03:43:55 +0600

    Create sheel command of task 7.

---
+++ task07.sh
@@ -0,0 +1,5 @@
+sudo chown :newgroup2 /home/shefat2002/os-concepts/*
+sudo chown :newgroup2 /home/shefat2002/ubuntu-is-the-best/*
+
+sudo ls -l /home/shefat2002/os-concepts
+sudo ls -l /home/shefat2002/ubuntu-is-the-best
